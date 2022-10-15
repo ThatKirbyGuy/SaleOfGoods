e6d717996b3c87c5efc5406db22044964adefea6
ThatKirbyGuy <ThatKirbyGuy25@gmail.com>
2022-10-15 11:23:01 +0300

    Добавьте файлы проекта.

---
+++ RadchenkovVladimir/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RadchenkovVladimir")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("RadchenkovVladimir")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f89c2759-cd79-479f-88af-1be2532b3930")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ VladimirRadchenkov/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VladimirRadchenkov
+{
+    public class Calc
+    {
+        public float Div(float a, float b)
+        {
+            return a / b;
+        }
+    }
+}
+
